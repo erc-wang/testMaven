00a5dda34fc0c52314d5cc70f4d224c87b0ca5e8
e-wang <86746723+erc-wang@users.noreply.github.com>
2023-06-07 16:57:31 -0400

    Created test note dummy

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,6 +4,7 @@ import org.joda.time.LocalTime;
 
 public class HelloWorld {
   public static void main(String[] args) {
+	//Test
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
     Greeter greeter = new Greeter();
